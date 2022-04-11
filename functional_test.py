111fafb9b06579bf4d6e1219d4f2df1f064cab04
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-04-11 13:46:05 +0200

    Incl second list entry to test

--- functional_test.py
+++ functional_test.py
@@ -32,12 +32,19 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy feather')
         inputbox.send_keys(Keys.ENTER)
-
-        time.sleep(5)
+        time.sleep(1)
         
+        # There is still a textbox to add another "fly home with feather" item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Fly home with feather')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # There is still a textbox to add another "fly home" item
+        
+        
+        # check for correct entries in todo list
         self.check_for_row_in_list_table('1: Buy feather')
+        self.check_for_row_in_list_table('2: Fly home with feather')
 
     
         """after adding, see a personal url and check that to-do is still there"""
