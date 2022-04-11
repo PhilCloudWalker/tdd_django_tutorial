cc11e57d2a32f92b1c5e6e1269c239c3b20f80ea
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-04-11 15:40:50 +0200

    Refactor selenium find_elemen(s)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from email import header
 from unicodedata import name
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 import time
 from django.test import LiveServerTestCase
@@ -16,19 +17,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertIn(row_text, [row.text for row in rows])
     
     def test_add_item_and_retrieve_later(self):
         """ToDo List in title and header"""
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
         """Add "buy feather" as item"""
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter To-Do item')
 
         inputbox.send_keys('Buy feather')
@@ -36,7 +37,7 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
         
         # There is still a textbox to add another "fly home with feather" item
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Fly home with feather')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
