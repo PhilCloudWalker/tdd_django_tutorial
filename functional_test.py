90f7f5ad394867c5602ad1ae24f73bc1a6b95a30
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-04-11 10:03:25 +0200

    Incl add a new item test in functional test

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
+from email import header
 from unicodedata import name
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,8 +17,21 @@ class NewVisitorTest(unittest.TestCase):
         """ToDo List in title and header"""
         self.browser.get('http://localhost:8000/')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header.text)
 
-        """Add "buy feather" and "make a fly" to as items"""
+        """Add "buy feather" as item"""
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter To-Do item')
+
+        inputbox.send_keys('Buy feather')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy feather' for row in rows))
+
+        # There is still a textbox to add another "fly home" item
 
     
         """after adding, see a personal url and check that to-do is still there"""
