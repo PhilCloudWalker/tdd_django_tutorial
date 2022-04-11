61c2d5b67d4abaf350779b5a49e146e6f272abd2
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-04-11 10:15:24 +0200

    Refactor home page

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.urls  import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+
 
 class HomePageTest(TestCase):
 
@@ -14,6 +16,5 @@ class HomePageTest(TestCase):
     def test_homepage_view_return_correct_values(self):
         request = HttpResponse()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.startswith(b'<html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
