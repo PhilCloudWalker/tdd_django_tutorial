97d9ddd2fba46c93c48511d0b49c1493bdd454c4
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-04-12 11:15:44 +0200

    Create List model and link to items

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ from lists.views import home_page
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -19,16 +19,24 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListandItemModelTest(TestCase):
 
     def test_saving_and_retrieving_item(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The 2nd item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -36,13 +44,16 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The 2nd item')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
-        Item.objects.create(text='item1')
-        Item.objects.create(text='item2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item1', list=list_)
+        Item.objects.create(text='item2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
